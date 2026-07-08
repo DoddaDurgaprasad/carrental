881033a55dbeffd0a7903603efd0317a359040ec
Durga Prasad Dodda <nanidodda7436@gmail.com>
2026-07-09 01:59:20 +0530

    smtp host rebuild

--- server/configs/mail.js
+++ server/configs/mail.js
@@ -4,9 +4,9 @@ dotenv.config();
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 465,
-  secure: true,
+  host: "smtp-relay.brevo.com", 
+  port: 2525,                 
+  secure: false,               
   auth: {
     user: process.env.EMAIL,
     pass: process.env.EMAIL_PASSWORD,
--- server/configs/redis.js
+++ server/configs/redis.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connection = new IORedis(process.env.REDIS_URL, {
+// Fallback to local 127.0.0.1 if process.env.REDIS_URL is blank or missing
+const redisUrl = process.env.REDIS_URL || "redis://127.0.0.1:6379";
+
+const connection = new IORedis(redisUrl, {
     maxRetriesPerRequest: null,
 }); 
 
 connection.on("connect", () => {
-    console.log("Redis Connected");
+    console.log("Redis Connected Successfully! 🚀");
 });
 
 connection.on("error", (err) => {
